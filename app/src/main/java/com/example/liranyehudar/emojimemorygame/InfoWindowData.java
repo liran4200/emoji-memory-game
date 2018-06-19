d176ccf7cc8cf7ed4e47735f964137580e2028a0
liran4200 <liran4200@gmail.com>
2018-06-19 18:16:20 +0300

    Added infoWindowData and CustomInfoWindowGoogleMap Classe for marker details, and marker layout.

---
+++ app/src/main/java/com/example/liranyehudar/emojimemorygame/InfoWindowData.java
@@ -0,0 +1,31 @@
+package com.example.liranyehudar.emojimemorygame;
+
+public class InfoWindowData {
+    private String name;
+    private String result;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
